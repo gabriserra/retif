12882e432f0c971fee39512886518c8cc7a97bc9
Gabriele Serra <gabriele_serra@hotmail.it>
2021-03-18 15:38:55 +0100

    Refactoring of test benches - Added attach test bench - Rework of benchmark sh to execute test with variable CPUs num

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,83 +1,193 @@
 #!/bin/bash
 
 ################################################################################
+# BENCHMARK TEST PLAN
+################################################################################
+
+# COMPARE ATTACH OVERHEAD
+# 0)
+# - Evaluate latency to call "sched_setscheduler" w/o daemon support
+# - Evaluate latency to "attach" a thread with given TID to a logic task
+# 
+# EVALUATE CREATE OVERHEAD
+# 1)
+# - Enable a set of plugins with a given set of CPUs
+# -- Vary task num (1-1024)
+# 2)
+# - Enable only EDF plugin performing 500 trials
+# -- Vary CPUs num (1-40)
+# -- Vary task num (1-1024)
+# 3)
+# - Enable only RM plugin performing 500 trials
+# -- Vary CPUs num (1-40)
+# -- Vary task num (1-1024)
+# 4)
+# - Enable only FP plugin performing 500 trials
+# -- Vary CPUs num (1-40)
+# -- Vary task num (1-1024)
+
+################################################################################
+# BENCHMARK FUNCTIONS
+################################################################################
+
+function benchmarks_setup() {
+    
+    if [ -d results ]; then
+        rm -rf results/*.csv
+    else
+        mkdir results
+    fi
+    
+    make clean
+    make
+}
+
+function generate_conf() {
+    # $1 test number
+    # $2 plugin name
+    # $3 cpu limit
+
+    if [ "$1" == 0 ] || [ "$1" == 1 ]; then
+        N=$1
+    else
+        N="n"
+    fi
+    
+    cp "$CWD/configs/benchmark$N.cfg" "$CWD/configs/schedconfig.cfg" 
+
+    if [ "$1" != 0 ] && [ "$1" != 1 ]; then
+        sed -i -e "s/PLG/$2/g" "$CWD/schedconfig.cfg"
+        sed -i -e "s/LIMIT/$3/g" "$CWD/schedconfig.cfg"
+    fi
+    
+    mv -f "$CWD/configs/schedconfig.cfg" "$SCHED_CFG" 
+}
+
+function do_benchmark () {
+    # $1 output file name
+
+    # to avoid interference with test tasks, run them using a lower priority
+    # nice -n +20 stress --cpu 4
+    
+    # bootstrap daemon (pin on CPU 1) and let it run in background
+    sudo taskset 0x00000010 chrt -r 99 rtsd 
+    DPID=$!
+
+    # execute benchmark 0 (pin on CPU 0)
+    sudo taskset 0x00000001 chrt -r 99 ./benchmark "$1"
+    sudo chown "$USER" "$1"
+
+    # tear down daemon
+    kill -INT $DPID
+}
+
+################################################################################
+# POWER SAVING / TURBO / HYPERTHREADING / MSR TSC MANAGEMENT
+################################################################################
+
+function load_msr() {
+    if ! lsmod | grep "msr" &> /dev/null ; then
+        sudo modprobe msr
+    fi
+}
+
+function disable_powersaving() {
+    # Sets "performance" as the new cpufreq governor
+    for CPU_FREQ_GOVERNOR in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; 
+    do [ -f "$CPU_FREQ_GOVERNOR" ] || continue;
+        sudo echo -n performance | sudo tee "$CPU_FREQ_GOVERNOR" > /dev/null;
+    done
+
+    # Displays CPU frequency for each core 
+    grep -E '^model name|^cpu MHz' /proc/cpuinfo
+}
+
+function disable_turbo() {
+    # Let's move to intel pstate directory
+    cd /sys/devices/system/cpu/intel_pstate || 
+        echo "Intel pstate not available" && return
+
+    FREQ_PERCENTAGE_1=$(($(cat max_perf_pct) - $(cat turbo_pct) - 5))
+    FREQ_PERCENTAGE_2=$(($(cat turbo_pct) - 5))
+
+    if [ $FREQ_PERCENTAGE_1 -lt $FREQ_PERCENTAGE_2 ] ; then
+        FREQ_PERCENTAGE=$FREQ_PERCENTAGE_1
+    else
+        FREQ_PERCENTAGE=$FREQ_PERCENTAGE_2
+    fi
+
+    # Disable turbo boost
+    echo 1 | sudo tee no_turbo > /dev/null
+
+    # And set cpu Pstate min and max to be equal
+    echo $FREQ_PERCENTAGE | sudo tee max_perf_pct > /dev/null
+    echo $FREQ_PERCENTAGE | sudo tee min_perf_pct > /dev/null
+
+    cd "$CWD" || exit
+}
+
+function disable_hyperthreading() {
+    # Which physical core is assigned to each logical core by checking
+    grep -H . /sys/devices/system/cpu/cpu*/topology/thread_siblings_list
+
+    Cores 0-4, 1-5, 2-6 and 3-7 are siblings, hence I will disable half of them
+    for i in {4..7}; do
+        echo -n 0 | sudo tee /sys/devices/system/cpu/cpu${i}/online > /dev/null;
+    done
+}
+
+################################################################################
 # SETUP
 ################################################################################
 
-FILE="output.csv"
+TEST_NUM=4
+MAX_CPU_NUM=8
+SCHED_CFG="/usr/share/rtsd/schedconfig.cfg"
 CWD=$(pwd)
+FILES=()
+
+for i in $(seq 0 $TEST_NUM); do FILES+=("results/benchmark$i.csv"); done
+
+#disable_powersaving
+#disable_turbo
+#disable_hyperthreading
+#load_msr
+benchmarks_setup
 
 ################################################################################
-# DISABLE POWER SAVING
+# PERFORM BENCHMARKS
 ################################################################################
 
-# Sets "performance" as the new cpufreq governor
-for CPU_FREQ_GOVERNOR in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; 
-do [ -f $CPU_FREQ_GOVERNOR ] || continue;
-    sudo echo -n performance | sudo tee $CPU_FREQ_GOVERNOR > /dev/null;
+# benchmark 0 -> attach time
+
+generate_conf 0
+do_benchmark "${FILES[0]}"
+
+# benchmark 1 -> fixed config
+
+generate_conf 1
+do_benchmark "${FILES[1]}"
+
+# benchmark 2 -> EDF with variable core num
+
+for i in $(seq $MAX_CPU_NUM); 
+do
+    generate_conf 2 EDF "$i"
+    do_benchmark "${FILES[2]}"
 done
 
-# Displays CPU frequency for each core 
-grep -E '^model name|^cpu MHz' /proc/cpuinfo
+# benchmark 3 -> RM with variable core num
 
-################################################################################
-# MODIFY TURBO SETTINGS
-################################################################################
+for i in $(seq $MAX_CPU_NUM); 
+do
+    generate_conf 3 RM "$i"
+    do_benchmark "${FILES[3]}"
+done
 
-# Let's move to intel pstate directory
-cd /sys/devices/system/cpu/intel_pstate
+# benchmark 4 -> FP with variable core num
 
-FREQ_PERCENTAGE_1=$(($(cat max_perf_pct) - $(cat turbo_pct) - 5))
-FREQ_PERCENTAGE_2=$(($(cat turbo_pct) - 5))
-
-if [ $FREQ_PERCENTAGE_1 -lt $FREQ_PERCENTAGE_2 ] ; then
-    FREQ_PERCENTAGE=$FREQ_PERCENTAGE_1
-else
-    FREQ_PERCENTAGE=$FREQ_PERCENTAGE_2
-fi
-
-# Disable turbo boost
-echo 1 | sudo tee no_turbo > /dev/null
-
-# And set cpu Pstate min and max to be equal
-echo $FREQ_PERCENTAGE | sudo tee max_perf_pct > /dev/null
-echo $FREQ_PERCENTAGE | sudo tee min_perf_pct > /dev/null
-
-cd $CWD
-
-################################################################################
-# DISABLE HYPERTHREADING
-################################################################################
-
-# Which physical core is assigned to each logical core by checking
-# grep -H . /sys/devices/system/cpu/cpu*/topology/thread_siblings_list
-
-# Cores 0-4, 1-5, 2-6 and 3-7 are siblings, hence I will disable half of them
-# for i in {4..7}; do
-    # echo -n 0 | sudo tee /sys/devices/system/cpu/cpu${i}/online > /dev/null;
-# done
-
-################################################################################
-# CLEAN & BUILD
-################################################################################
-rm $FILE
-make clean
-make
-
-################################################################################
-# LOAD MSR KERNEL MODULE
-################################################################################
-
-if ! lsmod | grep "msr" &> /dev/null ; then
-    sudo modprobe msr
-fi
-
-################################################################################
-# FORCE CPU USAGE & BENCHMARK
-################################################################################
-
-# to avoid interference with test tasks, run them using a lower priority
-# nice -n +20 stress --cpu 4
-
-sudo taskset 0x00000001 chrt -r 99 ./benchmark $FILE
-sudo chown $USER $FILE
+for i in $(seq $MAX_CPU_NUM); 
+do
+    generate_conf 4 RM "$i"
+    do_benchmark "${FILES[4]}"
+done
--- benchmark/benchmark_attach.c
+++ benchmark/benchmark_attach.c
@@ -1,7 +1,7 @@
 /**
- * This benchmark will spawn 1024 tasks and each of them will perform a
- * request to the daemon. The connection is performed only by the main process
- * hence the cost is not calculated at all. The output will be print into 
+ * This benchmark will spawn 4 tasks each of them specifies the preferred plugin
+ * and performs a request to the daemon. In this benchmark we want to evaluate
+ * the cost of attaching a thread and schedule it. The output will be print into 
  * the file passed as the first argument.
  */
 
@@ -11,19 +11,31 @@
 #include <librts.h>
 #include <errno.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 #include "rte_cycles.h"
 
-#define TSC
+#define T_NUM           4
 #define B_NUM           500
-#define T_NUM           1024
+
+#define EDF "EDF"
+#define RM  "RM"
+#define FP  "FP"
+#define RR  "RR"
 
 #define T_BUDGET_MIN    1
 #define T_BUDGET_MAX    4
 #define T_PERIOD_MIN    1024
 #define T_PERIOD_MAX    2048
+#define T_PRIORI_MIN    10
+#define T_PRIORI_MAX    50
 
 #define RAND_SEED       1
+
+volatile uint64_t spawned = 0;
+uint64_t tids[T_NUM];
 
 void print_err(char* message)
 {
@@ -37,6 +49,18 @@ uint32_t rand_bounded(uint32_t min, uint32_t max)
     return min + (rand() % max);
 }
 
+char* get_scheduler(int i)
+{
+    if (i == 0)
+        return EDF;
+    else if (i == 1)
+        return RM;
+    else if (i == 2)
+        return FP;
+    else
+        return RR;
+}
+
 void daemon_connect()
 {
     if (rts_daemon_connect() < 0)
@@ -45,29 +69,47 @@ void daemon_connect()
         printf("# Connected with daemon.\n");
 }
 
+void* do_something(void* t_id)
+{
+    tids[(long)t_id] = syscall(SYS_gettid);
+    spawned++;
+    while(1);
+
+    return NULL;
+}
+
 int main(int argc, char* argv[]) 
 {
     FILE* output;
     uint32_t ret;
     struct rts_params p;
     struct rts_task t[T_NUM];
+    pthread_t pt[T_NUM];
 
     if (argc < 2)
-        print_err("# Error - Wrong number of args.\n");
+    {
+        printf("# Error - Wrong number of args. Usage %s <filename>\n", argv[0]);
+        return -1;
+    }
 
     output = fopen(argv[1], "a");
 
     if (output == NULL)
         print_err("# Error - Unable to open output file.\n");
 
-    #ifdef TSC
     uint64_t tsc_before, tsc_after, tsc_freq;
     tsc_freq = rte_get_tsc_hz();
     printf("# TSC frequency: %ld\n", tsc_freq);
-    #endif
-
+    
     daemon_connect();
 
+    for (int i = 0; i < T_NUM; i++)
+        if (pthread_create(&pt[i], NULL, do_something, NULL) != 0)
+            print_err("# Error - Unable to create pthreads.\n");
+
+    // wait till other threads are ready
+    while(spawned != T_NUM);
+    
     printf("# Benchmark started ..... !\n");
     srand(RAND_SEED);
 
@@ -80,26 +122,23 @@ int main(int argc, char* argv[])
             rts_params_init(&p);
             rts_task_init(&(t[i]));
 
+            rts_params_set_scheduler(&p, get_scheduler(i));
             rts_params_set_period(&p, rand_bounded(T_PERIOD_MIN, T_PERIOD_MAX));
             rts_params_set_runtime(&p, rand_bounded(T_BUDGET_MIN, T_BUDGET_MAX));
+            rts_params_set_priority(&p, rand_bounded(T_PRIORI_MIN, T_PRIORI_MAX));
 
-            #ifndef TSC
-                struct timespec tp_before;
-                struct timespec tp_after;
-                clock_gettime(CLOCK_MONOTONIC_RAW, &tp_before);
-                rts_task_create(&(t[i]), &p);
-                clock_gettime(CLOCK_MONOTONIC_RAW, &tp_after);
-                fprintf(output, "%ld,", tp_after.tv_nsec - tp_before.tv_nsec);
-            #else
-                tsc_before = rte_get_tsc_cycles();
-                ret = rts_task_create(&(t[i]), &p);
-                tsc_after = rte_get_tsc_cycles();
-                fprintf(output, "%ld,", rte_get_tsc_elapsed(tsc_before, tsc_after, tsc_freq));
-            #endif
+            ret = rts_task_create(&(t[i]), &p);
 
             if (ret < 0)
-                print_err("# Benchmar error - Unable to create task");
+                print_err("# Benchmark error - Unable to create task");
 
+            tsc_before = rte_get_tsc_cycles();
+            ret = rts_task_attach(&(t[i]), tids[i]);
+            tsc_after = rte_get_tsc_cycles();
+            fprintf(output, "%ld,", rte_get_tsc_elapsed(tsc_before, tsc_after, tsc_freq));
+
+            if (ret < 0)
+                print_err("# Benchmark error - Unable to attach thread");
         }
 
         for (int i = 0; i < T_NUM; i++)
