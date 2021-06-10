6fbcbf9fa1cf445d323e6f34694e86669ee34133
Gabriele Ara <gabriele.ara@santannapisa.it>
2021-06-10 08:38:19 +0000

    Working on installing and packaging

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <librts.h>
+#include <retif.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
--- test/client.c
+++ test/client.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <librts.h>
+#include <retif.h>
 #include <errno.h>
 #include <string.h>
 #include <stdint.h>
