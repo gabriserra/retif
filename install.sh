45cbb2e1024951433d61514f5705cce2d214b903
Gabriele Ara <gabriele.ara@santannapisa.it>
2021-06-11 11:25:23 +0000

    Minor updates to build and install

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# -------------------------------------------------------- #
+
+function jump_and_print_path() {
+    cd -P "$(dirname "$1")" >/dev/null 2>&1 && pwd
+}
+
+function get_script_path() {
+    local _SOURCE
+    local _PATH
+
+    _SOURCE="${BASH_SOURCE[0]}"
+
+    # Resolve $_SOURCE until the file is no longer a symlink
+    while [ -h "$_SOURCE" ]; do
+        _PATH="$(jump_and_print_path "${_SOURCE}")"
+        _SOURCE="$(readlink "${_SOURCE}")"
+
+        # If $_SOURCE is a relative symlink, we need to
+        # resolve it relative to the path where the symlink
+        # file was located
+        [[ $_SOURCE != /* ]] && _SOURCE="${_PATH}/${_SOURCE}"
+    done
+
+    _PATH="$(jump_and_print_path "$_SOURCE")"
+    echo "${_PATH}"
+}
+
+# Argument: relative path of project directory wrt this
+# script directory
+function get_project_path() {
+    local _PATH
+    local _PROJPATH
+
+    _PATH=$(get_script_path)
+    _PROJPATH=$(realpath "${_PATH}/$1")
+    echo "${_PROJPATH}"
+}
+
+# -------------------------------------------------------- #
+
+(
+    set -e
+
+    path_proj="$(get_project_path ".")"
+
+    path_src="$path_proj"
+    path_build="$path_proj/build"
+
+    cmake --build "$path_build" --target install
+)
